196a19972aae74af595807828154152ff2d0c222
ehdwn4019 <ehdwn7531@naver.com>
2023-09-19 02:51:01 +0900

    Part4 : 게임 서버
    - Serialization #4

--- GameServer/Server/Server/ClientSession.cs
+++ GameServer/Server/Server/ClientSession.cs
@@ -24,6 +24,39 @@ namespace Server
         public long playerId;
         public string name;
 
+        public struct SkillInfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            //public bool Write(Span<byte> s, ref ushort count)
+            //{
+            //    bool success = true;
+            //
+            //    success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), id);
+            //    count += sizeof(int);
+            //    success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), level);
+            //    count += sizeof(short);
+            //    success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), duration);
+            //    count += sizeof(float);
+            //
+            //    return true;
+            //}
+
+            //public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            //{
+            //    id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+            //    count += sizeof(int);
+            //    level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+            //    count += sizeof(short);
+            //    duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+            //    count += sizeof(float);
+            //}
+        }   //
+
+        public List<SkillInfo> skills = new List<SkillInfo>();
+
         public PlayerInfoReq()
         {
             this.packetID = (ushort)PacketID.PlayerInfoReq;
@@ -46,6 +79,16 @@ namespace Server
             //ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             //count += sizeof(ushort);
             //this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+
+            skills.Clear();
+            //ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            //count += sizeof(ushort);
+            //for(int i=0; i<skillLen; i++)
+            //{
+            //    SkillInfo skill = new SkillInfo();
+            //    skill.Read(s, ref count);
+            //    skills.Add(skill);
+            //}
         }
 
         public override ArraySegment<byte> Write()
@@ -62,11 +105,27 @@ namespace Server
             //success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count, s.Count - count), packet.size);
             //count += sizeof(long);
 
+            //
             //ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
             //sucess &= BitConverter.TryWriteBytes(s.Slice(CountdownEvent, s.Length - count), nameLen);
             //count += sizeof(ushort);
             //Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, segment.Array, CountdownEvent, nameLen);
             //count += nameLen;
+
+            //위에거 개선하면 밑에거 
+
+            //ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+            //sucess &= BitConverter.TryWriteBytes(s.Slice(CountdownEvent, s.Length - count), nameLen);
+            //count += sizeof(ushort);
+            //count += nameLen;
+            //
+
+            //sucess &= BitConverter.TryWriteBytes(s.Slice(CountdownEvent, s.Length - count), (ushort)skills.Count);
+            //count += sizeof(ushort);
+            //foreach(SkillInfo skill in skills)
+            //{
+            //    success = &= skill.Write(s, ref count);
+            //}
 
             //success &= BitConverter.TryWriteBytes(s, count);
 
@@ -149,6 +208,11 @@ namespace Server
                         PlayerInfoReq p = new PlayerInfoReq();
                         p.Read(buffer);
                         Console.WriteLine($"PlayerInfoReq : {p.playerId} {p.name}");
+
+                        foreach(PlayerInfoReq.SkillInfo skill in p.skills)
+                        {
+                            Console.WriteLine($"Skill({skill.id})({skill.level})({skill.duration})");
+                        }
                     }
                     break;
                 case PacketID.PlayerInfoOK:
